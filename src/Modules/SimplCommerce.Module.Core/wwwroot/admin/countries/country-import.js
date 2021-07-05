e74a12f360b7550adb5dfec17fb88c8eb5b1491d
Mai Le Duong <maileduong92@gmail.com>
2021-07-05 16:32:12 +0700

    Add project files.

---
+++ src/Modules/SimplCommerce.Module.Core/wwwroot/admin/countries/country-import.js
@@ -0,0 +1,13 @@
+﻿
+(function (app) {
+    
+    app.controller('CountryImportController', CountryImportController);
+
+    CountryImportController.$inject = ['$scope'];
+
+    function CountryImportController($scope) {
+        var vm = this;
+
+        $scope.message = "Import data from excel file";
+    }
+})(angular.module('simplAdmin.core'));
